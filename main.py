780328823182c9197f65d850dce31b6aeda6302f
Pranav Khemka <pnv.khemka@gmail.com>
2023-12-30 17:08:59 +0545

    Added new Send Message to anyone Feature

--- main.py
+++ main.py
@@ -24,14 +24,13 @@ except:
 
 
 def sendDailyHomie():
-    attemptLabel = tk.Label(text="Attempting to send message to Homies")
-    attemptLabel.pack()
+    print("Option to send Message to Homies has been Selected")
     print("Getting USER ID")
     shubi_id = client.user_id_from_username('lazy_kedia')
     ans_id = client.user_id_from_username('anshulagarwal7838')
     esh_id = client.user_id_from_username('eshaan_bh')
     print(f"User IDs obtains\nShubi:{shubi_id}\nAns: {ans_id}\nEsh: {esh_id}")
-    print("Attemptimport timeing to send DM")
+    print("Attempting to send DM")
     try:
         client.direct_send("https://www.instagram.com/p/C0H6udbOH2d/?igsh=eHBvdGhneTA4b2No", [int(shubi_id)])
     except:
@@ -44,11 +43,33 @@ def sendDailyHomie():
         client.direct_send("https://www.instagram.com/p/C0H6udbOH2d/?igsh=eHBvdGhneTA4b2No", [int(ans_id)])
     except:
         print("DM Successfully sent to Eshaan")
-    successLabel = tk.Label(text="Message Succesfully Sent", fg="green")
+    successLabel = tk.Label(text="Message Succesfully Sent to Homies", fg="green")
     successLabel.pack()
 
+def sendMessage():
+    username = sendUsernameEntry.get()
+    message = sendEntry.get()
+    print("Option to send Message to a specified user has been selected")
+    user_id = client.user_id_from_username(username)
+    print(f"User ID of the {username} is {user_id}")
+    try:
+        client.direct_send(message, [int(user_id)])
+    except:
+        print(f"Succesfully sent message to {username}")
+        successLabel = tk.Label(text=f"Successfully sent Message to {username}", fg="green")
 
 sendDailyHomieButton = tk.Button(text="Send DailyHomieMessage to the Homies", command=sendDailyHomie)
 sendDailyHomieButton.pack()
+
+sendLabel = tk.Label(text="\n\n\nDo You Wish to send message to anyone else ??")
+sendLabel.pack()
+sendEntry = tk.Entry()
+sendEntry.pack()
+sendEntry.insert(1, "Insert Message Here")
+sendUsernameEntry = tk.Entry()
+sendUsernameEntry.pack()
+sendUsernameEntry.insert(1, "Insert Username of the User")
+sendButton = tk.Button(text="Send the Above Message", command=sendMessage)
+sendButton.pack()
 
 root.mainloop()
