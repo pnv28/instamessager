5ee1e1d445c7af6c39501415fada58c23b9fd94d
Pranav Khemka <pnv.khemka@gmail.com>
2023-12-31 13:22:04 +0545

    Add more expandibility + Spam Feature

---
+++ config.py
@@ -0,0 +1,4 @@
+username = "the_pnv28"
+password = "may8,rat"
+homies = ["lazy_kedia", "the_pnv28"]
+messageForHomies = "https://www.instagram.com/p/C0H6udbOH2d/?igsh=eHBvdGhneTA4b2No"
--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import tkinter.ttk as tkk
 from instagrapi import Client
 import time
 
+# Getting Stuff
+from config import username
+from config import password
+from config import homies
+from config import messageForHomies
+
 root = tk.Tk()
 root.title("Insta Messager")
 root.geometry("800x600")
@@ -11,38 +17,33 @@ try:
     loggedinLabel = tk.Label(text="Establishing connection to Instagram")
     loggedinLabel.pack()
     client = Client()
-    client.login(username='the_pnv28', password='may8,rat')
+    client.login(username=username, password=password)
     print(f"Succesfully logged in")
     loggedinLabel = tk.Label(text="Succesfully Established connection to Instagram", fg="green")
     loggedinLabel.pack()
 except:
     loggedinLabel= tk.Label(text="Connection Failed\nClosing in 5 seconds", fg="red")
     loggedinLabel.pack()
+    print("Could Not Connect to Instagram | Program Quitting in 5 seconds")
     time.sleep(5)
     root.destroy()
     exit()
 
-
 def sendDailyHomie():
-    print("Option to send Message to Homies has been Selected")
-    print("Getting USER ID")
-    shubi_id = client.user_id_from_username('lazy_kedia')
-    ans_id = client.user_id_from_username('anshulagarwal7838')
-    esh_id = client.user_id_from_username('eshaan_bh')
-    print(f"User IDs obtains\nShubi:{shubi_id}\nAns: {ans_id}\nEsh: {esh_id}")
-    print("Attempting to send DM")
+    print("Option to send Daily Message to Homie Selected")
     try:
-        client.direct_send("https://www.instagram.com/p/C0H6udbOH2d/?igsh=eHBvdGhneTA4b2No", [int(shubi_id)])
+        for i in homies:
+            try:
+                client.direct_send("This is a test message from pnv28's instagram messagers programs new Messaging System\nSorry for any distrubances", [int(client.user_id_from_username(i))])
+            except:
+                print(f"Message One Sent to {i}")
     except:
-        print("DM Succesfully sent to Shubi")
-    try:
-        client.direct_send("https://www.instagram.com/p/C0H6udbOH2d/?igsh=eHBvdGhneTA4b2No", [int(esh_id)])
-    except:
-        print("DM Succesfully sent to Anshul")
-    try:
-        client.direct_send("https://www.instagram.com/p/C0H6udbOH2d/?igsh=eHBvdGhneTA4b2No", [int(ans_id)])
-    except:
-        print("DM Successfully sent to Eshaan")
+        print("An Error Occured Program Quitting in 5 Seconds")
+        errorLabel = tk.Label(text="An Error Occured Quitting in 5 Seconds", fg="red")
+        errorLabel.pack()
+        time.sleep(5)
+        root.destroy()
+        exit()
     successLabel = tk.Label(text="Message Succesfully Sent to Homies", fg="green")
     successLabel.pack()
 
@@ -50,13 +51,34 @@ def sendMessage():
     username = sendUsernameEntry.get()
     message = sendEntry.get()
     print("Option to send Message to a specified user has been selected")
-    user_id = client.user_id_from_username(username)
-    print(f"User ID of the {username} is {user_id}")
     try:
-        client.direct_send(message, [int(user_id)])
+        client.direct_send(message, [int(client.user_id_from_username(username))])
     except:
         print(f"Succesfully sent message to {username}")
         successLabel = tk.Label(text=f"Successfully sent Message to {username}", fg="green")
+
+def spamMessage():
+    username = spamUsernameEntry.get()
+    message = spamMessageEntry.get()
+    spamCount = spamNumberEntry.get()
+    print("Option to Spam messages has Been Selected")
+    print(f"Task: To Spam {username}\nMessage {message}\nNumber of times {spamCount}")
+    userId = client.user_id_from_username(username)
+    try:
+        for i in range(int(spamCount)):
+            try:
+                message = message + spamCount
+                client.direct_send(message, [int(userId)])
+            except:
+                print(f"Message {i} Sent")
+    except:
+        print("An Error Occured Program Quitting in 5 Seconds")
+        errorLabel = tk.Label(text="An Error Occured Quitting in 5 Seconds", fg="red")
+        errorLabel.pack()
+        time.sleep(5)
+        root.destroy()
+        exit()
+    print(f"Spamming {username} Successfull")
 
 sendDailyHomieButton = tk.Button(text="Send DailyHomieMessage to the Homies", command=sendDailyHomie)
 sendDailyHomieButton.pack()
@@ -71,5 +93,22 @@ sendUsernameEntry.pack()
 sendUsernameEntry.insert(1, "Insert Username of the User")
 sendButton = tk.Button(text="Send the Above Message", command=sendMessage)
 sendButton.pack()
+
+spamLabel = tk.Label(text="\n\n\nSpamming is not Advisable but it is Sure Fun\nBelow is a Simple Insta Spamming Tool")
+spamLabel.pack()
+spamUsernameEntry = tk.Entry()
+spamUsernameEntry.pack()
+spamUsernameEntry.insert(1, "Username")
+spamMessageEntry = tk.Entry()
+spamMessageEntry.pack()
+spamMessageEntry.insert(1, "Message")
+spamNumberEntry = tk.Entry()
+spamNumberEntry.pack()
+spamNumberEntry.insert(1, "No. of Mesages")
+spamButton = tk.Button(text="Click Here to Initiate Spam", command=spamMessage)
+spamButton.pack()
+
+closeButton = tk.Button(text="Close Application | Do Not Use the Close button from System Menu Bar", command=exit)
+closeButton.pack()
 
 root.mainloop()
